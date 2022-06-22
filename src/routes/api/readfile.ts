7cd6ec0035b1b4527443017bf90da546d2a530ae
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-22 18:09:25 +0200

    finish task

--- src/routes/api/readfile.ts
+++ src/routes/api/readfile.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { promises as fsPromises } from 'fs';
+
 const readfile = express.Router()
 
 readfile.get('', (request: express.Request, response: express.Response, next: express.NextFunction) => {
-    response.status(200).json({
-        status: 1,
-        message: 'read'
-    })
+    fileRead()
+        .then(data => {
+            response.status(200).json({
+                status: 1,
+                data: data
+            })
+        }).catch(error => {
+            next(error);
+        })
 })
+
+async function fileRead() {
+    return await fsPromises.readFile('src/files/myFile.txt', 'utf-8')
+}
 
 export default readfile;
--- src/routes/api/writefile.ts
+++ src/routes/api/writefile.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+
+import { promises as fsPromises } from 'fs';
 const writefile = express.Router()
 
 writefile.post('', (request: express.Request, response: express.Response, next: express.NextFunction) => {
+    fileWrite(request.body.data)
     response.status(200).json({
         status: 1,
-        message: 'write'
+        message: 'file write successfully',
     })
 })
+
+async function fileWrite(data: string) {
+    const writeFile = await fsPromises.open('src/files/myFile.txt', 'a+');
+    await writeFile.write(`\n${data}`);
+    writeFile.close();
+}
 
 export default writefile;
