06ab8280b042ae865b246a40507b229d7980d9a7
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-22 16:01:09 +0200

    add nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
 import express from 'express';
 import 'dotenv/config'
 
+
+const app = express();
+const PORT = 8000;
+
+// #=======================================================================================#
+// #			                          run server                                       #
+// #=======================================================================================#
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`App Run at http://${process.env.HOST}:${process.env.PORT || PORT}`);
+});
+// #=======================================================================================#
+// #			                            router                                         #
+// #=======================================================================================#
+// app.use('', userRouter);
+// #=======================================================================================#
+// #			                        not Found middleware                               #
+// #=======================================================================================#
+app.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
+    response.status(404).json({
+        status: 0,
+        message: 'Not Found'
+    })
+})
+
+// #=======================================================================================#
+// #			                      error middleware                                     #
+// #=======================================================================================#
+app.use((error: any, request: express.Request, response: express.Response, next: express.NextFunction) => {
+    let status = error.status || 500;
+    response.status(status).json({
+        status: 0,
+        error: error.message + ''
+    })
+})
