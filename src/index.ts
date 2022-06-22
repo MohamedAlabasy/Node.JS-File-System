7f472b3edee61b7a7e4d70dfa4fce6954bdccaf9
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-22 16:22:09 +0200

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 
 import routes from './routes/index';
 import logger from './utilities/logger';
@@ -8,35 +8,48 @@ const app = express();
 const PORT = 8000;
 
 // #=======================================================================================#
-// #			                          run server                                       #
+// #			                          run server                                             #
 // #=======================================================================================#
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`App Run at http://${process.env.HOST}:${process.env.PORT || PORT}`);
+  console.log(
+    `App Run at http://${process.env.HOST}:${process.env.PORT || PORT}`
+  );
 });
 // #=======================================================================================#
-// #			                            router                                         #
+// #			                            router                                               #
 // #=======================================================================================#
 app.use('', logger, routes);
 
-
 // #=======================================================================================#
-// #			                        not Found middleware                               #
+// #			                        not Found middleware                                     #
 // #=======================================================================================#
-app.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
+app.use(
+  (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
     response.status(404).json({
-        status: 0,
-        message: 'Not Found'
-    })
-})
-
+      status: 0,
+      message: 'Not Found',
+    });
+  }
+);
 
 // #=======================================================================================#
-// #			                      error middleware                                     #
+// #			                      error middleware                                           #
 // #=======================================================================================#
-app.use((error: any, request: express.Request, response: express.Response, next: express.NextFunction) => {
-    let status = error.status || 500;
+app.use(
+  (
+    error: any,
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const status = error.status || 500;
     response.status(status).json({
-        status: 0,
-        error: error.message + ''
-    })
-})
+      status: 0,
+      error: error.message + '',
+    });
+  }
+);
